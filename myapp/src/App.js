582f34863a875860a235a5ec7de236ddd7c3231d
unknown <umairtahir2776@gmail.com>
2024-07-12 03:36:56 -0700

    first commit

---
+++ myapp/src/App.js
@@ -0,0 +1,24 @@
+import Prenav from "./Components/Prenav";
+import prenav from "./Components/prenav.css"
+import Logo from "./Components/Logo";
+import logo from "./Components/logo.css"
+import List from "./Components/List";
+import list from "./Components/list.css"
+import Swiper from 'swiper';
+import styles from "./Components/styles.css"
+// import Swiper styles
+import 'swiper/css';
+import Carousel from "./Components/Carousel";
+
+function App() {
+  return (
+    <>
+    <Prenav/>
+    <Logo/>
+    <List/>
+    <Carousel/>
+   </>
+  );
+}
+
+export default App;
